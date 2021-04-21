0db3a42b0f767c1b97f3af94547793a34a786f90
david cohen <davidcoh@g.jct.ac.il>
2021-04-22 00:44:43 +0300

    xml worke

--- src/unittests/renderer/RenderTests.java
+++ src/unittests/renderer/RenderTests.java
@@ -1,11 +1,15 @@
-/**
- * 
- */
 package unittests.renderer;
-import javax.swing.text.Document;
-import javax.xml.parsers.DocumentBuilderFactory;
 
+import static org.junit.Assert.fail;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import org.junit.Test;
+import org.w3c.dom.*;
 
 import elements.*;
 import geometries.*;
@@ -31,14 +35,18 @@ public class RenderTests {
 	public void basicRenderTwoColorTest() {
 
 		Scene scene = new Scene("Test scene")//
-				.setAmbientLight(new AmbientLight(new Color(255, 191, 191), 1))// 
+				.setAmbientLight(new AmbientLight(new Color(255, 191, 191), 1))//
 				.setBackground(new Color(75, 127, 90));
 
 		scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50),
-				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)), // up left
-				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up right
-				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)), // down left
-				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100))); // down right
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)), // up
+																													// left
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up
+																													// right
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)), // down
+																														// left
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100))); // down
+																													// right
 
 		ImageWriter imageWriter = new ImageWriter("base render test", 1000, 1000);
 		Render render = new Render() //
@@ -60,6 +68,33 @@ public class RenderTests {
 		Scene scene = new Scene("XML Test scene");
 		// enter XML file name and parse from XML file into scene object
 		// ...
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse("testXmlFile.xml");
+			doc.getDocumentElement().normalize();
+			NodeList sceneList = doc.getElementsByTagName(doc.getDocumentElement().getNodeName());
+			for (int i = 0; i < sceneList.getLength(); ++i) {
+				Node sceneNode = sceneList.item(i);
+				if (sceneNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) sceneNode;
+					List<Integer> list = stringToInt(element.getAttribute("background-color"));
+				    Color background = new Color(list.get(0),list.get(1),list.get(2));
+				    System.out.println(list.get(0)+ " " + list.get(1)+ " " +list.get(2));
+					//list = stringToInt(element.getAttribute("ambient-light color"));
+					//Color light = new Color(list.get(0),list.get(1),list.get(2));
+				//	Element elem = (Element)element.getElementsByTagName("sphere").item(0);
+			//		Geometries geo = new Geometries();
+					//if (elem.getNodeType() == Node.ELEMENT_NODE) {
+					//	double radius = Integer.parseInt(elem.getAttribute("radius"));
+						//list = stringToInt(elem.getAttribute("ambient-light color"));
+		//			}
+				}
+			}
+		} catch (Exception e) {
+			fail("cannot read the file");
+		}
+
 		ImageWriter imageWriter = new ImageWriter("xml render test", 1000, 1000);
 		Render render = new Render() //
 				.setImageWriter(imageWriter) //
@@ -71,8 +106,14 @@ public class RenderTests {
 		render.printGrid(100, new Color(java.awt.Color.YELLOW));
 		render.writeToImage();
 	}
+
+	public List<Integer> stringToInt(String str) {
+		Scanner scanner = new Scanner(str);
+		List<Integer> list = new LinkedList<Integer>();
+		while (scanner.hasNextInt()) {
+			list.add(scanner.nextInt());
+		}
+		return list;
+	}
+
 }
-
-	
-
-
