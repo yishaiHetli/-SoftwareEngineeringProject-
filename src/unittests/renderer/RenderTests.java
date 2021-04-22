e490cde1f2d8998f56c10912443ad1540dc1a0b4
david cohen <davidcoh@g.jct.ac.il>
2021-04-22 06:28:23 +0300

    finish xmlFile

--- src/unittests/renderer/RenderTests.java
+++ src/unittests/renderer/RenderTests.java
@@ -79,20 +79,29 @@ public class RenderTests {
 				if (sceneNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element element = (Element) sceneNode;
 					List<Integer> list = stringToInt(element.getAttribute("background-color"));
-				    Color background = new Color(list.get(0),list.get(1),list.get(2));
-				    System.out.println(list.get(0)+ " " + list.get(1)+ " " +list.get(2));
-					//list = stringToInt(element.getAttribute("ambient-light color"));
-					//Color light = new Color(list.get(0),list.get(1),list.get(2));
-				//	Element elem = (Element)element.getElementsByTagName("sphere").item(0);
-			//		Geometries geo = new Geometries();
-					//if (elem.getNodeType() == Node.ELEMENT_NODE) {
-					//	double radius = Integer.parseInt(elem.getAttribute("radius"));
-						//list = stringToInt(elem.getAttribute("ambient-light color"));
-		//			}
+					// System.out.print(list.get(0) + " " + list.get(1) + " " + list.get(2));
+					Color background = new Color(list.get(0), list.get(1), list.get(2));
+					list = stringToInt(getElement("ambient-light", "color", element, 0));
+					// System.out.print(list.get(0) + " " + list.get(1) + " " + list.get(2));
+					Color light = new Color(list.get(0), list.get(1), list.get(2));
+					scene.setBackground(background).setAmbientLight(new AmbientLight(light, 1));
+					double radius = Double.parseDouble(getElement("sphere", "radius", element, 0));
+					list = stringToInt(getElement("sphere", "center", element, 0));
+					scene.geometries.add(new Sphere(new Point3D(list.get(0), list.get(1), list.get(2)), radius));
+					int size = element.getElementsByTagName("triangle").getLength();
+					for (int j = 0; j < size; ++j) {
+						list = stringToInt(getElement("triangle", "p0", element, j));
+						Point3D p0 = new Point3D(list.get(0), list.get(1), list.get(2));
+						list = stringToInt(getElement("triangle", "p1", element, j));
+						Point3D p1 = new Point3D(list.get(0), list.get(1), list.get(2));
+						list = stringToInt(getElement("triangle", "p2", element, j));
+						Point3D p2 = new Point3D(list.get(0), list.get(1), list.get(2));
+						scene.geometries.add(new Triangle(p0, p1, p2));
+					}
 				}
 			}
 		} catch (Exception e) {
-			fail("cannot read the file");
+			fail(e.getMessage());
 		}
 
 		ImageWriter imageWriter = new ImageWriter("xml render test", 1000, 1000);
@@ -105,6 +114,11 @@ public class RenderTests {
 		render.renderImage();
 		render.printGrid(100, new Color(java.awt.Color.YELLOW));
 		render.writeToImage();
+	}
+
+	public String getElement(String tag, String attribute, Element element, int item) {
+		element = (Element) element.getElementsByTagName(tag).item(item);
+		return element.getAttribute(attribute);
 	}
 
 	public List<Integer> stringToInt(String str) {
