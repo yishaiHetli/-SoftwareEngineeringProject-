a1255e0a65a69946c79ae66bc48be81dc1772c31
yishai <yishai@DESKTOP-AKU18RB.lan>
2021-04-22 22:34:31 +0300

    Final 05\

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -1,16 +1,27 @@
 package elements;
 
 import primitives.*;
-
+/**
+ * class that calculate the light
+ * @author david&yishai
+ *
+ */
 public class AmbientLight {
 
 	private Color intensity;
-
+/**
+ * ctor calculate the intensity by multiply iA with kA
+ * @param iA  Color
+ * @param kA  double
+ */
 	public AmbientLight(Color iA, double kA) {
 
 		intensity = iA.scale(kA);
 	}
-
+/**
+ * 
+ * @return  intensity
+ */
 	public Color getIntensity() {
 		return intensity;
 	}
--- src/unittests/renderer/RenderTests.java
+++ src/unittests/renderer/RenderTests.java
@@ -20,7 +20,7 @@ import scene.*;
 /**
  * Test rendering a basic image
  * 
- * @author Dan
+ * @author david&yishai N.B(dan)
  */
 public class RenderTests {
 	private Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, -1), new Vector(0, -1, 0)) //
@@ -67,27 +67,33 @@ public class RenderTests {
 	public void basicRenderXml() {
 		Scene scene = new Scene("XML Test scene");
 		// enter XML file name and parse from XML file into scene object
-		// ...
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document doc = builder.parse("testXmlFile.xml");
 			doc.getDocumentElement().normalize();
 			NodeList sceneList = doc.getElementsByTagName(doc.getDocumentElement().getNodeName());
-			for (int i = 0; i < sceneList.getLength(); ++i) {
-				Node sceneNode = sceneList.item(i);
-				if (sceneNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element element = (Element) sceneNode;
+			for (int i = 0; i < sceneList.getLength(); ++i) {// loop of all the Scene Elements
+				Node sceneNode = sceneList.item(i); 
+				if (sceneNode.getNodeType() == Node.ELEMENT_NODE) {// if the sceneNode is type of Node Element
+					Element element = (Element) sceneNode;//convert sceneNode to Element
+					//value of the attribute from string to list of int
 					List<Integer> list = stringToInt(element.getAttribute("background-color"));
-					// System.out.print(list.get(0) + " " + list.get(1) + " " + list.get(2));
+					//create a new Color with the list value
 					Color background = new Color(list.get(0), list.get(1), list.get(2));
+					//get the Elements in type string and convert it to list of int
 					list = stringToInt(getElement("ambient-light", "color", element, 0));
-					// System.out.print(list.get(0) + " " + list.get(1) + " " + list.get(2));
+					//create a new Color with the list value
 					Color light = new Color(list.get(0), list.get(1), list.get(2));
+					//set the fields of scene. background and ambient Light 
 					scene.setBackground(background).setAmbientLight(new AmbientLight(light, 1));
+					//get the radius of sphere  
 					double radius = Double.parseDouble(getElement("sphere", "radius", element, 0));
+					//get the center point of sphere
 					list = stringToInt(getElement("sphere", "center", element, 0));
+					//add the sphere to the field geometries of scene 
 					scene.geometries.add(new Sphere(new Point3D(list.get(0), list.get(1), list.get(2)), radius));
+					//get the number of triangles
 					int size = element.getElementsByTagName("triangle").getLength();
 					for (int j = 0; j < size; ++j) {
 						list = stringToInt(getElement("triangle", "p0", element, j));
@@ -96,6 +102,7 @@ public class RenderTests {
 						Point3D p1 = new Point3D(list.get(0), list.get(1), list.get(2));
 						list = stringToInt(getElement("triangle", "p2", element, j));
 						Point3D p2 = new Point3D(list.get(0), list.get(1), list.get(2));
+						//add the triangle to the field geometries of scene 
 						scene.geometries.add(new Triangle(p0, p1, p2));
 					}
 				}
@@ -115,12 +122,23 @@ public class RenderTests {
 		render.printGrid(100, new Color(java.awt.Color.YELLOW));
 		render.writeToImage();
 	}
-
+/**
+ * 
+ * @param tag  name of tag Element
+ * @param attribute  name of tag Element Attribute 
+ * @param element  show of Element
+ * @param item  the number of the tag Element
+ * @return  string value of attribute of a specific tag element 
+ */
 	public String getElement(String tag, String attribute, Element element, int item) {
 		element = (Element) element.getElementsByTagName(tag).item(item);
 		return element.getAttribute(attribute);
 	}
-
+/**
+ * 
+ * @param str  the string we want to convert
+ * @return  list of string converted to int
+ */
 	public List<Integer> stringToInt(String str) {
 		Scanner scanner = new Scanner(str);
 		List<Integer> list = new LinkedList<Integer>();
@@ -129,5 +147,4 @@ public class RenderTests {
 		}
 		return list;
 	}
-
 }
